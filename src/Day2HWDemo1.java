1bd909c73e2a7351a123a6b074cd5481df04c65e
Office <Office@DESKTOP-8MQLQAH.fios-router.home>
2023-04-10 12:59:16 -0400

    Initial Commit Demo1

---
+++ src/Day2HWDemo1.java
@@ -0,0 +1,13 @@
+
+public class Day2HWDemo1 {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Happy Monday!");
+	}
+
+	public static void add(int a, int b) {
+		
+		System.out.println(a+b);
+	}
+}
